6cdf5cb7dcbbc658148d4d86935091e40cc36bce
TheMatw <hh_55555@naver.com>
2022-02-14 15:13:49 +0900

    django

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-path('', views.post_list, name='post_list'),
-    ]
+    # http://localhost:8000/blog
+    path('', views.post_list, name='post_list_home'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
-from blog.models import Post
+from .models import Post
 
-# Create your views here.
-# view 내에 선언된 함수로 인자로 HttpRequest라는 객체를 Django가 전달해준다.
+# 글상세정보
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
+# Views 내에 선언된 함수로 인자로 HttpRequest 라는 객체를 Django가 전달해준다.
+# 글목록
 def post_list(request):
     my_name = '장고웹프레임워크'
     http_method = request.method
 
     # return HttpResponse('''
     #     <h2>Welcome {name}</h2>
-    #     <p>Http Method : {method}<p/>
-    #     <p>Http headers user-Agent : {header}</p>
+    #     <p>Http Method : {method}</p>
+    #     <p>Http headers User-Agent : {header}</p>
     #     <p>Http Path : {mypath}</p>
-    # '''.format(name=my_name, method=http_method, header=request.headers['user_agent'], mypath=request.path))
+    # '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], mypath=request.path))
 
     # return render(request, 'blog/post_list.html')
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html',{'post_list':posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'post_list': posts})
