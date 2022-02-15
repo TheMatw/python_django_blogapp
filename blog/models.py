112027d37e6bd1d619a4de9bcbc7a6ffb016d1bf
TheMatw <hh_55555@naver.com>
2022-02-15 13:11:51 +0900

    django

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django import forms
+
+# title 필드의 length가 2보다 작으면 검증오류 발생시키는 함수 선언하기
+def min_length_2_validatro(value):
+    if len(value) < 2:
+        # validataionError 예외를 강제로 발생시킨다
+        raise forms.ValidationError('title은 2글자 이상 입력해주세요!')
 
 # Create your models here.
 class Post(models.Model):
-    # 작성자
+
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    # 제목
-    title = models.CharField(max_length=200)
-    # 내용
+    title = models.CharField(max_length=200, validators=[min_length_2_validatro])
     text = models.TextField()
-    # 작성일
     created_date = models.DateTimeField(default=timezone.now)
-    # 게시일
     published_date = models.DateTimeField(blank=True, null=True)
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/blog
     path('', views.post_list, name='post_list_home'),
+# http://localhost:8000/blog/post/5
     path('post/<int:pk>', views.post_detail, name='post_detail'),
+    # http://localhost:8000/blog/post/new
+    path('host/new', views.post_new, name='post_new'),
+
 ]
