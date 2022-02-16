44e398c1602eaeeec8f79db306dc9a62fe6d5d48
TheMatw <hh_55555@naver.com>
2022-02-16 09:52:59 +0900

    django

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/blog
     path('', views.post_list, name='post_list_home'),
-# http://localhost:8000/blog/post/5
+    # http://localhost:8000/blog/post/5
     path('post/<int:pk>', views.post_detail, name='post_detail'),
     # http://localhost:8000/blog/post/new
-    path('host/new', views.post_new, name='post_new'),
+    path('post/new', views.post_new, name='post_new'),
+    # http://localhost:8000/blog/post/1/edit
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,16 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostModelForm, PostForm
 
-# 글등록 (form) 사용
+# 글수정(ModelForm) 사용
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        pass
+    else:
+        form = PostModelForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'postform': form})
+
+# 글등록(Form) 사용
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -13,39 +22,39 @@ def post_new(request):
             # 검증을 통과한 입력 데이터
             print(form.cleaned_data)
             clean_data_dict = form.cleaned_data
-            # create() 함수가 호출되면 등록처리가 이루어진다
+            # create() 함수가 호출되면 등록처리가 이루어진다.
             post = Post.objects.create(
-                author = request.user,
+                author=request.user,
                 title = clean_data_dict['title'],
-                text = clean_data_dict['text'],
-                published_date = timezone.now()
+                text=clean_data_dict['text'],
+                published_date=timezone.now()
             )
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'postform':form})
+    return render(request, 'blog/post_edit.html', {'postform': form})
 
-
-# 글등록 (ModelForm사용)
+# 글등록 (ModelForm 사용)
 def post_new_modelform(request):
     if request.method == 'POST':
         # 등록을 요청하는 경우
         post_form = PostModelForm(request.POST)
+        # 검증로직을 통과하면
         if post_form.is_valid():
-            # form 객체의 save() 호출하면 Model 객체가 생성됨
+            # form 객체의 save() 호출하면 Model 객체가 생성되어진다.
             post = post_form.save(commit=False)
-            # 로그인된 username을 작성자(author) 필드에 저장
+            # 로그인된 username을 작성자(author)필드에 저장
             post.author = request.user
             # 현재날짜시간을 게시일자(published_date) 필드에 저장
             post.published_date = timezone.now()
-            # post 객체가 저장되면서 insert 처리가 되어진다
+            # post 객체가 저장되면서 insert 처리가 되어진다.
             post.save()
             # 등록 후에 상세페이지로 리다이렉션 처리하기
             return redirect('post_detail', pk=post.pk)
     else:
         # 등록을 위한 Form을 출력하는 경우
         post_form = PostModelForm()
-    return render(request, 'blog/post_edit.html', {'postform' : post_form})
+    return render(request, 'blog/post_edit.html', {'postform': post_form})
 
 # 글상세정보
 def post_detail(request,pk):
